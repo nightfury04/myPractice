f27a14362c357c0a247a19abcb915fec855f6a75
Rahul <rastogi04.rahul@gmail.com>
2016-03-05 13:08:45 +0530

    Enums

--- src/com/exceptions/TestingExceptions.java
+++ src/com/exceptions/TestingExceptions.java
@@ -8,9 +8,11 @@ public class TestingExceptions {
 	public static void main(String[] args) {
 		
 	try {
+		System.out.println("Running application");
 		throw new MyException();
 	} catch (MyException e) {
 		// TODO Auto-generated catch block
+		System.out.println(e.getMessage());
 	}
 	
 	}
