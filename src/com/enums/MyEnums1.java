eabe504873ccac9a90211d79e5f260b9f90872d5
Rahul <rastogi04.rahul@gmail.com>
2016-03-05 13:13:15 +0530

    enums

---
+++ src/com/enums/MyEnums1.java
@@ -0,0 +1,30 @@
+package com.enums;
+
+public class MyEnums1 {
+	
+	public static void main(String[] args)
+	{
+		Days d1 = Days.MON;
+		System.out.println("Day of week: "+d1.getDayOfWeek());
+	}
+	
+	
+
+}
+
+
+enum Days
+{
+	MON(1),TUE(2),WED(3),THUR(4),FRI(5),SAT(6),SUN(7);
+	
+	int dayOfWeek;
+	private Days(int dayOfWeek)
+	{
+		this.dayOfWeek = dayOfWeek;
+	}
+	
+	public int getDayOfWeek()
+	{
+		return dayOfWeek;
+	}
+}
